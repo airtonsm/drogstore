6b027ce587b677b5097cdfc54ba418151af604f4
José Airton <airtonsmorais28@gmail.com>
2021-03-13 21:51:32 -0300

    Update UsuarioController.java

--- src/main/java/com/drogstore/controller/UsuarioController.java
+++ src/main/java/com/drogstore/controller/UsuarioController.java
@@ -2,6 +2,7 @@ package com.drogstore.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,7 +19,8 @@ public class UsuarioController {
 	@RequestMapping(method = RequestMethod.GET, value = "/cadastro_usuario")
 	public ModelAndView cadastroUsuario() {
 		ModelAndView modelAndView =  new ModelAndView("cadastro/cadastro_usuario");
-		
+		Iterable<Usuario> usuariosIt = usuarioRepository.findAll();
+		modelAndView.addObject("usuarios", usuariosIt);
 		return modelAndView;
 	}
 	
@@ -26,10 +28,26 @@ public class UsuarioController {
 	public ModelAndView salvar(Usuario usuario) {
 		
 		ModelAndView model = new ModelAndView("cadastro/cadastro_usuario");
-		
+		Iterable<Usuario> usuariosIt = usuarioRepository.findAll();
+		model.addObject("usuarios", usuariosIt);
+		model.addObject("usuarioobj", new Usuario());
 		usuarioRepository.save(usuario);
 		
 		return model;
 	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/listarusuarios")
+	public ModelAndView usuarios() {
+		ModelAndView modelAndView = new ModelAndView("cadastro/cadastro_usuario");
+		Iterable<Usuario> usuariosIt = usuarioRepository.findAll();
+		modelAndView.addObject("usuarios", usuariosIt);
+		return modelAndView;
+	}
+	
+	//public ModelAndView editar(@PathVariable("idusuario") Long idpessoa) {
+		
+		
+		
+	//}
 
 }
