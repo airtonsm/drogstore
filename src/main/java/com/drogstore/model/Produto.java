6c5bb087316fe34389dd4e8c9f3af6731da3a986
José Airton <airtonsmorais28@gmail.com>
2021-03-17 20:23:05 -0300

    Create Produto.java

---
+++ src/main/java/com/drogstore/model/Produto.java
@@ -0,0 +1,32 @@
+package com.drogstore.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Produto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    private double preco;
+
+
+
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+}
