971ceece17ec2fdb0b86e008cc568ac95ecfcb51
José Airton <airtonsmorais28@gmail.com>
2021-03-17 23:05:07 -0300

    create relationship beetween table

--- src/main/java/com/drogstore/model/Pedido.java
+++ src/main/java/com/drogstore/model/Pedido.java
@@ -20,8 +20,19 @@ public class Pedido implements Serializable {
     @ManyToOne
     private Usuario usuario;
 
-    @OneToMany
+    @OneToMany(fetch =FetchType.EAGER)
+    @JoinTable(name="pedido_produtos", uniqueConstraints = @UniqueConstraint(
+            columnNames = {"id_pedido", "id_produto"}, name = "unique_pedido_produto"),
+    joinColumns = @JoinColumn(name = "id_pedido",
+            referencedColumnName = "id",
+            table = "pedido", unique = false,
+    foreignKey = @ForeignKey(name = "pedido_fk,", value = ConstraintMode.CONSTRAINT)),
+
+    inverseJoinColumns = @JoinColumn(name = "produto_id", unique = false ,
+            referencedColumnName = "id", updatable = false,
+    table = "produto", foreignKey = @ForeignKey(name = "produto_fk", value = ConstraintMode.CONSTRAINT)))
     private List<Produto> produto;
+
 
     public void setId(Long id) {
         this.id = id;
