f463c6798075702ad7b0d83b957fbc8f37a98f2e
José Airton <airtonsmorais28@gmail.com>
2021-03-20 23:43:47 -0300

    Update Cliente.java

--- src/main/java/com/drogstore/model/Cliente.java
+++ src/main/java/com/drogstore/model/Cliente.java
@@ -9,23 +9,20 @@ import java.io.Serializable;
 @Entity
 public class Cliente implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private String nome;
     private String cpf;
     private String contato;
-    private String email;
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
 
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getNome() {
@@ -51,14 +48,4 @@ public class Cliente implements Serializable {
     public void setContato(String contato) {
         this.contato = contato;
     }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-
 }
