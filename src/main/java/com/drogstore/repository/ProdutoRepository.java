9d7e3cd5b9dec158a339c2d352252c2fc014a7ec
José Airton <airtonsmorais28@gmail.com>
2021-03-21 22:59:41 -0300

    Create ProdutoRepository.java

---
+++ src/main/java/com/drogstore/repository/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package com.drogstore.repository;
+
+import com.drogstore.model.Produto;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends CrudRepository<Produto, Long> {
+
+}
