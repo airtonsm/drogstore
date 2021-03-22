a2f6b9a52345cf9d2f4ab4db9728ceae594f20ca
José Airton <airtonsmorais28@gmail.com>
2021-03-21 22:59:32 -0300

    Update ProdutoController.java

--- src/main/java/com/drogstore/controller/ProdutoController.java
+++ src/main/java/com/drogstore/controller/ProdutoController.java
@@ -1,5 +1,8 @@
 package com.drogstore.controller;
 
+import com.drogstore.model.Produto;
+import com.drogstore.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,9 +12,21 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
     public class ProdutoController {
 
+        @Autowired
+        private ProdutoRepository produtoRepository;
+
         @RequestMapping(method = RequestMethod.GET, value = "/cadastro_produto")
         public ModelAndView inicio(){
             ModelAndView model = new ModelAndView("/cadastro/cadastro_produto");
+            return model;
+        }
+
+        @RequestMapping(method = RequestMethod.POST, value = "/salvarproduto")
+        public ModelAndView salvar(Produto produto){
+
+            ModelAndView model = new ModelAndView("cadastro/cadastro_produto");
+            produtoRepository.save(produto);
+
             return model;
         }
 
