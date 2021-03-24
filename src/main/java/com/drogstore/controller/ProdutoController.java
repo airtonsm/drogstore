c93633df7143c8d5aa3dc115f29e129ca62489dc
José Airton <airtonsmorais28@gmail.com>
2021-03-23 21:54:13 -0300

    Search method added

--- src/main/java/com/drogstore/controller/ProdutoController.java
+++ src/main/java/com/drogstore/controller/ProdutoController.java
@@ -4,9 +4,14 @@ import com.drogstore.model.Produto;
 import com.drogstore.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Controller
@@ -29,6 +34,19 @@ import org.springframework.web.servlet.ModelAndView;
 
             return model;
         }
+
+        @PostMapping("**/pesquisarMed")
+            public ModelAndView pesquisar(@RequestParam("medPesquisa") String medPesquisa){
+
+                List<Produto> produtos = new ArrayList<Produto>();
+
+                produtos = produtoRepository.findProdutoByNome(medPesquisa);
+
+                ModelAndView model = new ModelAndView("vendas/pedido");
+                model.addObject("produtos", produtos);
+
+                return model;
+            }
 
 
     }
