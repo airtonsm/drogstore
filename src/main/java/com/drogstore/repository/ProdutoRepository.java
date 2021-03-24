936fcc28408d579711cdc927e8edbe4a44fefea9
José Airton <airtonsmorais28@gmail.com>
2021-03-23 21:56:50 -0300

    Query per name the product

--- src/main/java/com/drogstore/repository/ProdutoRepository.java
+++ src/main/java/com/drogstore/repository/ProdutoRepository.java
@@ -1,10 +1,16 @@
 package com.drogstore.repository;
 
 import com.drogstore.model.Produto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProdutoRepository extends CrudRepository<Produto, Long> {
+
+    @Query("select p from Produto p where p.nome like %?1%")
+    List<Produto> findProdutoByNome(String nome);
 
 }
