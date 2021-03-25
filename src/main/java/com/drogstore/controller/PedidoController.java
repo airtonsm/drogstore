caa88c77e0415f1d9d149d048094558188489f56
José Airton <airtonsmorais28@gmail.com>
2021-03-24 21:58:49 -0300

    Update PedidoController.java

--- src/main/java/com/drogstore/controller/PedidoController.java
+++ src/main/java/com/drogstore/controller/PedidoController.java
@@ -5,11 +5,14 @@ import com.drogstore.model.Produto;
 import com.drogstore.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PedidoController {
@@ -23,12 +26,18 @@ public class PedidoController {
         return model;
     }
 
-//    public ModelAndView listar(Pedido pedido, Produto produto){
-//
-//        produtoRepository.findById(produto.getId());
-//
-//
-//
-//    }
+
+    @PostMapping("**/pesquisarMed")
+    public ModelAndView pesquisar(@RequestParam("medPesquisa") String medPesquisa){
+
+        List<Produto> produtos = new ArrayList<Produto>();
+
+        produtos = produtoRepository.findProdutoByNome(medPesquisa);
+
+        ModelAndView model = new ModelAndView("vendas/pedido");
+        model.addObject("produtos", produtos);
+
+        return model;
+    }
 
 }
